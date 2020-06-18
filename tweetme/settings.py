5a94bde5d21118af2eb2a07c45ee2f44a1f16e3d
Nelson Bui <nelson.bui923@gmail.com>
2020-06-17 22:02:36 -0700

    settings updated

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ezqovj@w%msdtw6$ayrthk_3lq%$7-lhz_$*^&eij3x+bx2e5+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'tweetme-app.herokuapp.com']
 LOGIN_URL = "/login"
 
 MAX_TWEET_LENGTH = 240
